dc2c89689888356c904839512de1ded6431a375a
panteliselef <panteliselef@outlook.com>
2024-06-30 15:20:42 +0300

    chore: Prepare package for release

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,3 @@
-import { Accessor, Component, createComputed, createSignal } from 'solid-js'
-
-export function createHello(): [Accessor<string>, (to: string) => void] {
-  const [hello, setHello] = createSignal('Hello World!')
-
-  return [hello, (to: string) => setHello(`Hello ${to}!`)]
-}
-
-export const Hello: Component<{ to?: string }> = props => {
-  const [hello, setHello] = createHello()
-
-  createComputed(() => {
-    if (typeof props.to === 'string') setHello(props.to)
-  })
-
-  return (
-    <>
-      <div>{hello()}</div>
-    </>
-  )
-}
-
 export { Clerk } from './clerk'
 export * from './hooks'
 export * from './components/uiComponents'
